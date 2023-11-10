115d8b18ebc5adaccd0573fd83d705828c27c3c2
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2023-11-10 16:10:10 +0700

    Add `get_iso_*` functions

--- R/get.R
+++ R/get.R
@@ -23,222 +23,177 @@
 #' @export
 #' @rdname get-functions
 get_country_name <- function(key, unmatched = NA) {
-
-  inds <- dict_countries$mrio_ind
-  names <- dict_countries$mrio_name |> tolower()
-  codes <- dict_countries$mrio_code |> tolower()
-
   result <- c()
-
   for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_countries$mrio_name[match])
-      }
+    match <- match_country(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
     } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_countries$mrio_name[match])
-      }
-    }
-  }
+      result <- c(result, dict_countries$mrio_name[match])
+    }}
   return(result)
 }
 
 #' @export
 #' @rdname get-functions
 get_country_ind <- function(key, unmatched = NA) {
-
-  inds <- dict_countries$mrio_ind
-  names <- dict_countries$mrio_name |> tolower()
-  codes <- dict_countries$mrio_code |> tolower()
-
   result <- c()
-
   for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, inds[match])
-      }
+    match <- match_country(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
     } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, inds[match])
-      }
-    }
-  }
+      result <- c(result, dict_countries$mrio_ind[match])
+    }}
   return(result)
 }
 
 #' @export
 #' @rdname get-functions
 get_country_code <- function(key, unmatched = NA) {
-
-  inds <- dict_countries$mrio_ind
-  names <- dict_countries$mrio_name |> tolower()
-  codes <- dict_countries$mrio_code |> tolower()
-
   result <- c()
-
   for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_countries$mrio_code[match])
-      }
+    match <- match_country(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
     } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_countries$mrio_code[match])
-      }
-    }
-  }
+      result <- c(result, dict_countries$mrio_code[match])
+    }}
+  return(result)
+}
+
+#' @export
+#' @rdname get-functions
+get_iso_num <- function(key, unmatched = NA) {
+  result <- c()
+  for (k in key) {
+    match <- match_country(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
+    } else {
+      result <- c(result, dict_countries$iso_num[match])
+    }}
+  return(result)
+}
+
+#' @export
+#' @rdname get-functions
+get_iso_a2 <- function(key, unmatched = NA) {
+  result <- c()
+  for (k in key) {
+    match <- match_country(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
+    } else {
+      result <- c(result, dict_countries$iso_a2[match])
+    }}
+  return(result)
+}
+
+#' @export
+#' @rdname get-functions
+get_iso_a3 <- function(key, unmatched = NA) {
+  result <- c()
+  for (k in key) {
+    match <- match_country(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
+    } else {
+      result <- c(result, dict_countries$iso_a3[match])
+    }}
   return(result)
 }
 
 #' @export
 #' @rdname get-functions
 get_sector_name <- function(key, unmatched = NA) {
-
-  inds <- dict_sectors$c_ind
-  names <- dict_sectors$c_name |> tolower()
-  shortnames <- dict_sectors$c_name_short |> tolower()
-  codes <- dict_sectors$c_abv |> tolower()
-
   result <- c()
-
   for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(shortnames == k), which(codes == k))
-      match <- c(which(names == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_sectors$c_name[match])
-      }
+    match <- match_sector(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
     } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_sectors$c_name[match])
-      }
-    }
-  }
+      result <- c(result, dict_sectors$c_name[match])
+    }}
   return(result)
 }
 
 #' @export
 #' @rdname get-functions
 get_sector_shortname <- function(key, unmatched = NA) {
-
-  inds <- dict_sectors$c_ind
-  names <- dict_sectors$c_name |> tolower()
-  shortnames <- dict_sectors$c_name_short |> tolower()
-  codes <- dict_sectors$c_abv |> tolower()
-
   result <- c()
-
   for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(shortnames == k), which(codes == k))
-      match <- c(which(names == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_sectors$c_name_short[match])
-      }
+    match <- match_sector(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
     } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_sectors$c_name_short[match])
-      }
-    }
-  }
+      result <- c(result, dict_sectors$c_name_short[match])
+    }}
   return(result)
 }
 
 #' @export
 #' @rdname get-functions
 get_sector_ind <- function(key, unmatched = NA) {
-
-  inds <- dict_sectors$c_ind
-  names <- dict_sectors$c_name |> tolower()
-  shortnames <- dict_sectors$c_name_short |> tolower()
-  codes <- dict_sectors$c_abv |> tolower()
-
   result <- c()
-
   for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(shortnames == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, inds[match])
-      }
+    match <- match_sector(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
     } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, inds[match])
-      }
-    }
-  }
+      result <- c(result, dict_sectors$c_ind[match])
+    }}
   return(result)
 }
 
 #' @export
 #' @rdname get-functions
 get_sector_code <- function(key, unmatched = NA) {
+  result <- c()
+  for (k in key) {
+    match <- match_sector(k)
+    if (length(match) == 0) {
+      result <- c(result, unmatched)
+    } else {
+      result <- c(result, dict_sectors$c_abv[match])
+    }}
+  return(result)
+}
+
+# Helper functions --------------------------------------------------------
+
+match_country <- function(j) {
+
+  inds <- dict_countries$mrio_ind
+  names <- dict_countries$mrio_name |> tolower()
+  codes <- dict_countries$mrio_code |> tolower()
+  iso_num <- dict_countries$iso_num
+  iso_a2 <- dict_countries$iso_a2 |> tolower()
+  iso_a3 <- dict_countries$iso_a3 |> tolower()
+
+  if (is.na(as.numeric(j)) |> suppressWarnings()) {
+    j <- tolower(j)
+    match <- c(which(names == j), which(codes == j), which(iso_a2 == j), which(iso_a3 == j))
+  } else {
+    match <- c(which(inds == as.numeric(j)), which(iso_num == as.numeric(j)))
+  }
+
+  return(match[1])
+}
+
+match_sector <- function(j) {
 
   inds <- dict_sectors$c_ind
   names <- dict_sectors$c_name |> tolower()
   shortnames <- dict_sectors$c_name_short |> tolower()
   codes <- dict_sectors$c_abv |> tolower()
 
-  result <- c()
-
-  for (k in key) {
-    if (is.na(as.numeric(k)) |> suppressWarnings()) {
-      k <- tolower(k)
-      match <- c(which(names == k), which(shortnames == k), which(codes == k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_sectors$c_abv[match])
-      }
-    } else {
-      match <- which(inds == as.numeric(k))
-      if (length(match) == 0) {
-        result <- c(result, unmatched)
-      } else {
-        result <- c(result, dict_sectors$c_abv[match])
-      }
-    }
+  if (is.na(as.numeric(j)) |> suppressWarnings()) {
+    j <- tolower(j)
+    match <- c(which(names == j), which(shortnames == j), which(codes == j))
+  } else {
+    match <- which(inds == as.numeric(j))
   }
-  return(result)
+
+  return(match[1])
 }
